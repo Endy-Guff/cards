9ad4c28f64d6e01ce98db836e4c1dcc1a53bacec
Endy-Guff <andreymaranchak@yandex.ru>
2023-08-03 21:02:11 +0300

    add pagination component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -8,14 +8,16 @@ import { Typography } from '../typography'
 
 import s from './select.module.scss'
 
-type Option = { label: string; value: string }
+type Option = { label: string; value: string } | { label: number; value: string }
 type SelectPropsType = {
   placeholder: string
   options: Option[]
   onChange?: (value: string) => void
   value?: string
-  label: string
+  label?: string
   disabled?: boolean
+  width?: number
+  height?: number
 }
 export const Select: FC<SelectPropsType> = ({
   placeholder,
@@ -24,10 +26,12 @@ export const Select: FC<SelectPropsType> = ({
   label,
   onChange,
   value,
+  width,
+  height,
 }) => {
   const [open, setOpen] = useState<boolean>(false)
   const mappedOptions = options.map(op => {
-    return <SelectItem key={op.value} option={op} />
+    return <SelectItem key={op.value} option={op} width={width} height={height} />
   })
   const disabledClass = disabled ? s.disabled : ''
 
@@ -45,7 +49,7 @@ export const Select: FC<SelectPropsType> = ({
             {label}
           </Typography.Body2>
         </span>
-        <SelectRadix.Trigger className={s.trigger}>
+        <SelectRadix.Trigger className={s.trigger} style={{ width: width, height: height }}>
           <SelectRadix.Value placeholder={<Typography.Body1>{placeholder}</Typography.Body1>} />
           <SelectRadix.Icon className={`${s.icon} ${open && s.iconOpen}`}>
             <DownIcon size={16} color={'var(--color-light-100)'} disabled={disabled} />
@@ -53,7 +57,7 @@ export const Select: FC<SelectPropsType> = ({
         </SelectRadix.Trigger>
 
         <SelectRadix.Portal>
-          <SelectRadix.Content position={'popper'} className={s.content}>
+          <SelectRadix.Content position={'popper'} className={s.content} style={{ width: width }}>
             <SelectRadix.Viewport>
               <SelectRadix.Group>{mappedOptions}</SelectRadix.Group>
             </SelectRadix.Viewport>
@@ -66,12 +70,18 @@ export const Select: FC<SelectPropsType> = ({
 
 type SelectItemPropsType = {
   option: Option
+  width?: number
+  height?: number
 }
-const SelectItem: FC<SelectItemPropsType> = ({ option }) => {
+const SelectItem: FC<SelectItemPropsType> = ({ option, width, height }) => {
   const { label, value } = option
 
   return (
-    <SelectRadix.Item value={value} className={s.option}>
+    <SelectRadix.Item
+      value={value.toString()}
+      className={s.option}
+      style={{ width: width, height: height }}
+    >
       <SelectRadix.ItemText>
         <Typography.Body1>{label}</Typography.Body1>
       </SelectRadix.ItemText>
