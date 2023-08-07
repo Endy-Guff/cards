0d4e49eb22d564396655db5599905fc9953eb9f3
Endy-Guff <andreymaranchak@yandex.ru>
2023-08-07 16:38:27 +0300

    add table component

---
+++ src/components/ui/table/table.stories.tsx
@@ -0,0 +1,124 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { TableMenu } from '../tableMenu'
+
+import { Table } from './table.tsx'
+
+const meta = {
+  title: 'Components/Table',
+  component: Table.Root,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Table.Root>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: args => <Table.Root {...args} />,
+
+  args: {
+    children: (
+      <>
+        <Table.Head>
+          <Table.Row>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Cards</Table.HeadCell>
+            <Table.HeadCell>Last Updated</Table.HeadCell>
+            <Table.HeadCell>Created by</Table.HeadCell>
+            <Table.HeadCell></Table.HeadCell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Pack Name 1</Table.Cell>
+            <Table.Cell>4</Table.Cell>
+            <Table.Cell>18.03.2021</Table.Cell>
+            <Table.Cell>Ivan Ivanov</Table.Cell>
+            <Table.Cell>
+              <TableMenu
+                id={1}
+                deleteCallback={() => {}}
+                changeCallback={() => {}}
+                playCallback={() => {}}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Pack Name 2</Table.Cell>
+            <Table.Cell>4</Table.Cell>
+            <Table.Cell>18.03.2021</Table.Cell>
+            <Table.Cell>Ivan Ivanov</Table.Cell>
+            <Table.Cell>
+              <TableMenu
+                id={1}
+                deleteCallback={() => {}}
+                changeCallback={() => {}}
+                playCallback={() => {}}
+              />
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Pack Name 3</Table.Cell>
+            <Table.Cell>4</Table.Cell>
+            <Table.Cell>18.03.2021</Table.Cell>
+            <Table.Cell>Ivan Ivanov</Table.Cell>
+            <Table.Cell>
+              <TableMenu
+                id={1}
+                deleteCallback={() => {}}
+                changeCallback={() => {}}
+                playCallback={() => {}}
+              />
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </>
+    ),
+  },
+}
+
+const data = [
+  { id: 1, name: 'Pack Name 1', cardsCount: 4, lastUpdated: '18.03.2021', creator: 'Ivan Ivanov' },
+  { id: 2, name: 'Pack Name 2', cardsCount: 4, lastUpdated: '18.03.2021', creator: 'Ivan Ivanov' },
+  { id: 3, name: 'Pack Name 3', cardsCount: 4, lastUpdated: '18.03.2021', creator: 'Ivan Ivanov' },
+]
+
+export const TableWithMappedData: Story = {
+  render: args => <Table.Root {...args} />,
+
+  args: {
+    children: (
+      <>
+        <Table.Head>
+          <Table.Row>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Cards</Table.HeadCell>
+            <Table.HeadCell>Last Updated</Table.HeadCell>
+            <Table.HeadCell>Created by</Table.HeadCell>
+            <Table.HeadCell></Table.HeadCell>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          {data.map(item => {
+            return (
+              <Table.Row key={item.id}>
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>{item.cardsCount}</Table.Cell>
+                <Table.Cell>{item.lastUpdated}</Table.Cell>
+                <Table.Cell>{item.creator}</Table.Cell>
+                <Table.Cell>
+                  <TableMenu
+                    id={item.id}
+                    deleteCallback={() => {}}
+                    changeCallback={() => {}}
+                    playCallback={() => {}}
+                  />
+                </Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </>
+    ),
+  },
+}
---
+++ src/components/ui/table/table.tsx
@@ -0,0 +1,79 @@
+import { ComponentProps, FC } from 'react'
+
+import { Typography } from '../typography'
+
+import s from './table.module.scss'
+
+export type RootProps = ComponentProps<'table'>
+
+export const Root: FC<RootProps> = ({ className, ...rest }) => {
+  const classNames = {
+    table: `${className} ${s.table}`,
+  }
+
+  return <table className={classNames.table} {...rest} />
+}
+
+export type HeadProps = ComponentProps<'thead'>
+
+export const Head: FC<HeadProps> = props => {
+  return <thead {...props} />
+}
+
+export type BodyProps = ComponentProps<'tbody'>
+
+export const Body: FC<BodyProps> = props => {
+  return <tbody {...props} />
+}
+
+export type RowProps = ComponentProps<'tr'>
+
+export const Row: FC<RowProps> = props => {
+  return <tr {...props} />
+}
+
+export type HeadCellProps = ComponentProps<'th'>
+
+export const HeadCell: FC<HeadCellProps> = ({ className, ...rest }) => {
+  const classNames = {
+    headCell: `${className} ${s.headCell}`,
+  }
+
+  return <th className={classNames.headCell} {...rest} />
+}
+
+export type CellProps = ComponentProps<'td'>
+
+export const Cell: FC<CellProps> = ({ className, ...rest }) => {
+  const classNames = {
+    cell: `${className} ${s.tableCell}`,
+  }
+
+  return <td className={classNames.cell} {...rest} />
+}
+
+export const Empty: FC<ComponentProps<'div'> & { mt?: string; mb?: string }> = ({
+  className,
+  mt = '89px',
+  mb,
+}) => {
+  const classNames = {
+    empty: `${className} ${s.empty}`,
+  }
+
+  return (
+    <Typography.H2 className={classNames.empty} style={{ marginTop: mt, marginBottom: mb }}>
+      Данных нет
+    </Typography.H2>
+  )
+}
+
+export const Table = {
+  Root,
+  Head,
+  Body,
+  Row,
+  HeadCell,
+  Cell,
+  Empty,
+}
